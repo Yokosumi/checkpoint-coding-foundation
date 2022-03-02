ef3cce41879483946bfa1555c5450a9dfcdc1810
Selman Ciftci <selman.ciftci.coding@gmail.com>
2022-03-02 12:19:50 +0100

    add: applied solution to challenge

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,13 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  const arrA = strA.split(""); // [a,b,c]
+  const arrB = strB.split(""); // [1,2,3]
+
+  let mergedArr = [];
+
+  for (let i = 0; i < arrA.length || i < arrB.length; i++) {
+    mergedArr.push(arrA[i]);
+    mergedArr.push(arrB[i]);
+  }
+
+  return mergedArr.join("");
 }
